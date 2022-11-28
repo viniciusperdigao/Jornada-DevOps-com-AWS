a4843a4642de8d4007243613fdf4a5e85ddd0b27
Vinicius <eng.viniciusperdigao@gmail.com>
2022-11-28 19:45:54 -0300

    alteração do diretório

--- Desafio de Projeto - Linux/iac2.sh
+++ Desafio de Projeto - Linux/iac2.sh
@@ -17,6 +17,6 @@ echo "Baixando e copiando os arquivos da aplicação..."
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html/
 
